9ddf831289f84f94c8725b8eb8c8c9f0942e0749
V4n1sh795 <v4n1shnerush@gmail.com>
2025-09-21 20:40:53 +0500

    check previous commmit

--- task3.py
+++ task3.py
@@ -1,36 +1,54 @@
+import json
+import bs4
 import re
-import csv
 
-input_csv = input()
-new_file = input()
-key_words = input().split(',')
+html = input()
 
-def first_step(text):
-    result = re.sub(r'<[^>]*>', '', text)
-    result = re.sub(r'(\d{4})-(\d{2})-(\d{2})T(\d{2})[.:](\d{2}):(\d{2})\+\d{4}', r'\4:\5:\6 \3/\2/\1', result)
-    result = re.sub(r'(?<=\s)(\d{2})\.(\d{2})(?=\s)', r'\1:\2', result)
-    result = re.sub(r'(?<=^)(\d{2})\.(\d{2})(?=\s)', r'\1:\2', result)
-    result = re.sub(r'(?<=\s)(\d{2})\.(\d{2})(?=$)', r'\1:\2', result) 
-    return result
+exchange = {
+    '₽': 1.0,
+    '$': 100.0,
+    '€': 105.0,
+    '₸': 0.210,
+    'Br': 30.0,
+}
 
-def highlight_keywords(text, keywords):
-    def replace_func(match):
-        word = match.group(0)
-        for keyword in keywords:
-            if keyword.lower() in word.lower():
-                return word.upper()
-        return word
-    return re.sub(r'\b\w+[\'\w]*\b', replace_func, text)
+result = {
+    'vacancy': None,
+    'salary': None,
+    'experience': None,
+    'company': None,
+    'description': None,
+    'skills': None,
+    'created_at': None,
+}
 
-with open(input_csv, 'r', encoding='utf-8') as inp, open(new_file, 'w', encoding='utf-8', newline='') as out:
-    reader = csv.reader(inp)
-    writer = csv.writer(out)    
-    
-    for row in reader:
-        processed_row = []
-        for cell in row:
-            result = first_step(cell)
-            result = highlight_keywords(result, key_words)
-            processed_row.append(result)
-        
-        writer.writerow(processed_row)
+soup = bs4.BeautifulSoup(open(html), "html.parser")
+
+
+vacancy_title = soup.find(class_='vacancy-title')
+salary = vacancy_title.find(attrs={"data-qa": 'vacancy-salary'}).text
+result['salary'] = re.findall(r'\d+', salary)
+result['vacancy'] = vacancy_title.find('h1').text
+
+vacancy_description_list_item = soup.find(class_='vacancy-description-list-item').text
+exp = re.findall(r'(\d+)–(\d+)', vacancy_description_list_item) # [['1', '3']]
+result['experience'] = f'{exp[0][0]}-{exp[0][1]}'
+
+vacancy_company_name = soup.find(class_="vacancy-company-name").text
+result['company'] = vacancy_company_name
+
+description_element = soup.find(attrs={"data-qa": "vacancy-description"}).text
+result['description'] = description_element
+
+all_desc = soup.find_all(class_='vacancy-description')
+for desc in all_desc:
+    try:
+        key_skills = desc.find_all(class_='vacancy-section')
+    except Exception as e:
+        continue
+span_skills = [x for x in key_skills][2].find_all('span')
+result['skills'] = '; '.join([x.text for x in span_skills])
+
+date = soup.find(class_='vacancy-creation-time-redesigned').text
+result['created_at'] = re.search(r'\d.*\d', date).group().replace('\xa0', ' ')
+print(json.dumps(result))
